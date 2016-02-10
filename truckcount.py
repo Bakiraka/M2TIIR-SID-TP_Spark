3fd3fc370b4619ae59fd7fcb3646b3f373c434d7
bakiraka <anthony.djenane@fostyboy.net>
2016-02-10 09:05:29 +0000

    modification algo

--- truckcount.py
+++ truckcount.py
@@ -6,7 +6,7 @@ from operator import add
 sc = SparkContext(appName="truckcount")
 
 # File reading
-text_file = sc.textFile("/home/debian/data/truck/generalvehicle.txt")
+text_file = sc.textFile("/home/debian/data/data/generalvehicle.txt")
 
 #filter out header
 file_header = text_file.first()
@@ -17,9 +17,9 @@ counted_tuples = data.map(lambda line: line.split("\t")) \
                      .filter(lambda x: x[60] > 0) \
                      .map(lambda x: x[5]) \
                      .map(lambda x: (x,1)) \
-                     .reduceByKey(add)
+                     .reduceByKey(add) \
+                     .collect()
 
-sum(pair[1] for pair in counted_tuples)
+print sum(pair[1] for pair in counted_tuples)
 
-counts.saveAsTextFile("~/count")
 
